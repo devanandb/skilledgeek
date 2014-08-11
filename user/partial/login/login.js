17bf0ad8231c25163a58af266e319b5d507da8d7
Devanand <dev.ryder88@gmail.com>
2014-08-11 11:52:30 +0530

    Completed homepage and global styles

--- user/partial/login/login.js
+++ user/partial/login/login.js
@@ -7,4 +7,37 @@ angular.module('user').controller('LoginCtrl',function($scope, $rootScope, $loca
 
 	};
 
+	$scope.message=false;
+
+	$scope.user = $rootScope.user;
+
+	$scope.login = function(){
+		if ($scope.user.pass !== $scope.user.password) {
+			$scope.message = true;
+			$scope.user.pass = "";
+		}
+		else{
+			$location.path('/dashboard');
+		}
+	};
+
+	$scope.statusDeck = true;
+
+	$scope.hideDeck = function(statusTab) {
+		$scope.statusDeck = false;
+		$scope.activeTab = statusTab;
+	};
+
+	$scope.showDeck = function() {
+		$scope.statusDeck = true;
+		$scope.activeTab = "";
+	};
+
+	
+	$scope.forgotmessage = false;
+	$scope.forgotPass = function(){
+		$scope.forgotmessage = true;
+	};
+
+
 });
